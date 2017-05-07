4bb4beffca1548c8582e3d8d9aefe472f602e327
Michael Chen <miketychen@gmail.com>
2017-05-07 13:23:41 -0700

    add barebone starterkit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# reactjs-starter
+# ReactJS Starter Kit
+This is a very simply ReactJS build to generate static sites with React & SASS
---
+++ app/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>ReactJS Starter Kit</h3>
+      </div>
+    );
+  }
+};
+
+export default Home;
